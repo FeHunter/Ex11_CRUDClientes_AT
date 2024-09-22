0b8c3ae7b2c53338e662b50c87477afd77599b25
FeHunter <hunterfe1111@gmail.com>
2024-09-22 17:07:38 -0300

    Adicionado Controller para clientes usando scarfold...

--- Ex11_CRUDClientes_AT/Data/Contexto.cs
+++ Ex11_CRUDClientes_AT/Data/Contexto.cs
@@ -5,7 +5,7 @@ namespace Ex11_CRUDClientes_AT.Data
 {
     public class Contexto : DbContext
     {
-        DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     }
 }
