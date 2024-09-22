a30504c34eacb8e3f15ec2b33dfbe9f90d889764
FeHunter <hunterfe1111@gmail.com>
2024-09-22 16:56:51 -0300

    Adicionar arquivos de projeto.

---
+++ Ex11_CRUDClientes_AT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ex11_CRUDClientes_AT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
