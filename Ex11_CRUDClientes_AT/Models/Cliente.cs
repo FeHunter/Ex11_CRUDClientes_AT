20eef76f16934a6836008ffddb967796e236637b
FeHunter <hunterfe1111@gmail.com>
2024-09-22 17:06:09 -0300

    Criada migrations e database

--- Ex11_CRUDClientes_AT/Models/Cliente.cs
+++ Ex11_CRUDClientes_AT/Models/Cliente.cs
@@ -4,6 +4,8 @@ namespace Ex11_CRUDClientes_AT.Models
 {
     public class Cliente
     {
+        [Key]
+        public int Id { get; set; }
         [Required(ErrorMessage = "Nome do cliente e obrigatório")]
         public string Nome { get; set; }
         [Required(ErrorMessage = "Sobrenome do cliente e obrigatório")]
