944079bc32126814b65ae5a7c46e8de4ba9c376c
FeHunter <hunterfe1111@gmail.com>
2024-09-22 16:59:55 -0300

    Criada classe cliente

---
+++ Ex11_CRUDClientes_AT/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ex11_CRUDClientes_AT.Models
+{
+    public class Cliente
+    {
+        [Required(ErrorMessage = "Nome do cliente e obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Sobrenome do cliente e obrigatório")]
+        public string Sobrenome { get; set; }
+        [Required(ErrorMessage = "Email é obrigatório")]
+        public string Email { get; set; }
+    }
+}
